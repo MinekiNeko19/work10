c2592a7b1589984e829d0e40c6385b76ae236abb
Sarah Leong <sleong20@stuy.edu>
2021-10-15 13:09:10 -0400

    added headers and includes

---
+++ linked.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct cat {
+    char name[100];
+    int age;
+    char breed[100];
+    struct cat *next;
+    };
+
+void print_cat(struct cat *c);
+struct cat * add_cat(char n[20], int a, char b[20]);
+void print_list(struct cat *);
+struct cat * insert_front(struct cat *, int);
+struct cat * free_list(struct cat *);
+
